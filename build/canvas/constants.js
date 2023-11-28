cfd013dc28025e39efcf73cdb72e095ccdf07c5e
Evan Williams <evanw555@gmail.com>
2023-11-28 00:44:18 -0800

    Fix default canvas palette bug

--- build/canvas/constants.js
+++ build/canvas/constants.js
@@ -4,7 +4,7 @@ exports.DEFAULT_GRAPH_PALETTE = void 0;
 // The Discord color palette
 exports.DEFAULT_GRAPH_PALETTE = {
     background: '#36393e',
-    padding: '#282b3',
+    padding: '#282b30',
     lightPadding: '#424549',
     darkPadding: '#1e2124',
     highlight: '#7289da',
